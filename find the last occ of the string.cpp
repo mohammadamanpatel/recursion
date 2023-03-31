d6ec50ee0095a612985b2c8c005607c9621d28fd
mohammadamanpatel <125564920+mohammadamanpatel@users.noreply.github.com>
2023-03-31 16:53:18 +0530

    Update find the last occ of the string.cpp

--- find the last occ of the string.cpp
+++ find the last occ of the string.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 
 using namespace std;
@@ -24,3 +22,29 @@ int main()
     cout<<ans<<endl;
     return 0;
 }
+
+// #include <iostream>
+
+// using namespace std;
+// void findLasOcc(int i,char target,int length,string s,int &ans)
+// {
+//     if(i<0)
+//     {
+//       return;
+//     }
+//     if(s[i]==target){
+//        ans = i;
+//        return;
+//     }
+//     findLasOcc(i-1,target,length,s,ans);
+// }
+// int main()
+// {
+//     string s = "aman";
+//     int i=s.length();
+//     char target = 'a';
+//     int ans;
+//     findLasOcc(i,target,s.size(),s,ans);
+//     cout<<ans<<endl;
+//     return 0;
+// }
