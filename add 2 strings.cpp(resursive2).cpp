c58f6d7afe0a6a3f81dcfada29eb0af280aa93f6
mohammadamanpatel <125564920+mohammadamanpatel@users.noreply.github.com>
2023-04-01 17:14:19 +0530

    Create add 2 strings.cpp(resursive2).cpp

---
+++ add 2 strings.cpp(resursive2).cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void addition(string &num1,string &num2,int i,int j, int carry,string &result)
+    {
+        //base case
+        if(i<0 and j<0)
+        {
+            if(carry!=0)
+            {
+                result.push_back(carry + '0');
+            }
+            return;
+        }
+
+        //ek case
+
+        int a = (i>=0 ? num1[i] : '0') - '0';
+        int b = (j>=0 ? num2[j] : '0') - '0';
+        int c = a + b + carry;
+        int digit = c%10;
+        carry = c/10;
+        result.push_back(digit + '0'); 
+
+        //recursive case
+        
+        addition(num1,num2,i-1,j-1,carry,result);
+    }
+    string addStrings(string num1, string num2) {
+        string ans = "";
+        int carry = 0;
+        addition(num1,num2,num1.size()-1,num2.size()-1,carry,ans);
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
